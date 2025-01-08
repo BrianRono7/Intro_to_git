4193348688228df989f98387edadc71c386462ac
eng-Brono <engbriankrono@gmail.com>
2025-01-08 20:13:16 +0300

    Updating the code lines

--- Word Frequency.py
+++ Word Frequency.py
@@ -16,3 +16,7 @@ def word_frequency(sentence):
 
 input = input("Please enter a sentence:")
 print(word_frequency(input))
+
+
+# 2. Write a function that takes a list of numbers and returns the sum of the squares of all the numbers.
+# 3. Write a function that takes a list of numbers and returns the sum of the squares of all the numbers.
